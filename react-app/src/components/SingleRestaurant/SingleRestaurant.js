ae447c4f41b4f0b61dd92e1b124f3aa031537b1e
Ryan <rjrose.19@gmail.com>
2023-10-04 12:51:33 -0400

    basic CRUD for restaurants

--- react-app/src/components/SingleRestaurant/SingleRestaurant.js
+++ react-app/src/components/SingleRestaurant/SingleRestaurant.js
@@ -2,6 +2,9 @@ import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { getSelectedRestaurant } from "../../store/restaurants"
 import { useParams } from 'react-router-dom'
+import UpdateRestaurantForm from '../UpdateRestaurantForm/UpdateRestaurantForm'
+import DeleteRestaurant from '../DeleteRestaurant/DeleteRestaurant'
+import OpenModalButton from "../OpenModalButton"
 
 const SingleRestaurant = () => {
     const dispatch = useDispatch()
@@ -19,6 +22,14 @@ const SingleRestaurant = () => {
             <div>{ restaurant.city }</div>
             <div>{ restaurant.state }</div>
             <div>{ restaurant.hours }</div>
+            <OpenModalButton
+                buttonText="Update Restaurant"
+                modalComponent={<UpdateRestaurantForm />}
+            />
+            <OpenModalButton
+                buttonText="Delete Restaurant"
+                modalComponent={<DeleteRestaurant restaurant={restaurant}/>}
+            />
         </div>
     )
 }
